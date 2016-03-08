4a5667c325a6c853c18272fdc87e1614c3d83b31
adityag3 <adityagovil3@gmail.com>
2016-03-08 13:56:07 +0530

    Basic Templates added

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.http import Http404
+from .models import Question
+# To load the created template instead of the default template
+from django.template import loader
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Hello World. Welcome to polling website.")
+	latest_question_list = Question.objets.order_by('-pub_date')[:5]
+	template = loader.get_template('polls/index.html')
+	context = {
+		'latest_question_list' : latest_question_list
+	}
+	#output = ', '.join([q.question_text for q in latest_question_list])
+	#return HttpResponse(output)
+
+	return render(request,template,context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/detail.html', {'question': question})
+    #return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
